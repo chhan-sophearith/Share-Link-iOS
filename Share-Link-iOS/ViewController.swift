b29fe1e5685bcefc92324dae7c82cf405c2b2e30
Chhan Sophearith <44798886+chhan-sophearith@users.noreply.github.com>
2023-07-05 11:45:59 +0700

    init

--- Share-Link-iOS/ViewController.swift
+++ Share-Link-iOS/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapShareLink(_ sender: UIButton) {
+        
+        let descriptionTitle =
+            """
+            Click below link to open youtube: \("https://www.youtube.com/") from App Store or Play Store.
+            """
+        let sharedContents = [
+            descriptionTitle
+        ]
+        
+        let activityViewController = UIActivityViewController(activityItems: sharedContents,
+                                                              applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
 }
 
